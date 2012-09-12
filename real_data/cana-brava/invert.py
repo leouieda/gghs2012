95313c7e4e97d46e52bd8353b810390034089d14
Leonardo Uieda <leouieda@gmail.com>
2012-09-12 18:43:55 -0300

    Started running shape inversion for Cana Brava

---
+++ real_data/cana-brava/invert.py
@@ -0,0 +1,66 @@
+import cPickle as pickle
+import fatiando as ft
+import numpy as np
+
+log = ft.log.tofile(ft.log.get(), 'invert.log')
+log.info(ft.log.header())
+
+outcropy, outcropx = np.loadtxt('outcrop.txt', unpack=True)*111000
+lon, lat, height, data = np.loadtxt('gravity-anomaly-residual.dat', unpack=True)
+z = -height
+y, x = lon*111000, lat*111000
+
+bounds = (x.min(), x.max(), y.min(), y.max(), z.min() + 1, 10000)
+mesh = ft.msh.ddd.PrismMesh(bounds, (20, 100, 100))
+mesh.carvetopo(x, y, height)
+
+dms = ft.pot.harvester.wrapdata(mesh, x, y, z, gz=data)
+
+seeds = ft.pot.harvester.sow(ft.pot.harvester.loadseeds('seeds.json'),
+    mesh, mu=1, delta=0.0001)
+
+ft.vis.figure3d()
+ft.vis.prisms([s.get_prism() for s in seeds], 'density')
+ft.vis.vtk.mlab.plot3d(outcropx, outcropy, z.min()*np.ones_like(outcropx),
+    color=(1,0,0), tube_radius=300)
+ft.vis.axes3d(ft.vis.outline3d(bounds), fmt='%.1f', nlabels=3,
+    ranges=[lat.min(), lat.max(), lon.min(), lon.max(), bounds[-2]*0.001,
+        bounds[-1]*0.001])
+ft.vis.wall_bottom(bounds)
+ft.vis.wall_north(bounds)
+ft.vis.show3d()
+
+estimate, goals, misfits = ft.pot.harvester.harvest(dms, seeds)
+mesh.addprop('density', estimate['density'])
+predicted = dms[0].get_predicted()
+
+with open('result.pickle', 'w') as f:
+    pickle.dump(mesh, f)
+with open('seeds.pickle', 'w') as f:
+    pickle.dump([s.get_prism() for s in seeds], f)
+np.savetxt('predicted.txt', np.transpose([lon, lat, predicted]))
+
+shape = (100, 100)
+ft.vis.figure()
+ft.vis.title("True: color | Inversion: contour")
+ft.vis.axis('scaled')
+levels = ft.vis.contourf(lon, lat, data, shape, 12, interp=True)
+ft.vis.colorbar()
+ft.vis.contour(lon, lat, predicted, shape, levels, color='k', interp=True)
+ft.vis.xlabel('Longitude')
+ft.vis.ylabel('Latitude')
+ft.vis.m2km()
+ft.vis.show()
+
+ft.vis.figure3d()
+ft.vis.prisms([s.get_prism() for s in seeds], 'density')
+ft.vis.prisms(ft.msh.ddd.vremove(0, 'density', mesh), 'density')
+#ft.vis.prisms(mesh, 'density')
+ft.vis.vtk.mlab.plot3d(outcropx, outcropy, z.min()*np.ones_like(outcropx),
+    color=(1,0,0), tube_radius=300)
+ft.vis.axes3d(ft.vis.outline3d(bounds), fmt='%.1f', nlabels=3,
+    ranges=[lat.min(), lat.max(), lon.min(), lon.max(), bounds[-2]*0.001,
+        bounds[-1]*0.001])
+ft.vis.wall_bottom(bounds)
+ft.vis.wall_north(bounds)
+ft.vis.show3d()
---
+++ real_data/cana-brava/pickseeds.py
@@ -0,0 +1,39 @@
+import fatiando as ft
+import numpy as np
+import simplejson as json
+
+log = ft.log.get()
+log.info(ft.log.header())
+
+lon, lat, height, data = np.loadtxt('gravity-anomaly-residual.dat', unpack=True)
+outcropx, outcropy = np.loadtxt('outcrop.txt', unpack=True)
+area = [lon.min(), lon.max(), lat.min(), lat.max()]
+
+dens_larger = 270
+dens_smaller = 390
+
+ft.vis.figure()
+ft.vis.suptitle("Pick the seeds for the larger portion")
+ft.vis.axis('scaled')
+ft.vis.contourf(lon, lat, data, (100, 100), 80, interp=True)
+ft.vis.colorbar()
+ft.vis.plot(outcropx, outcropy, '-k')
+# x is north = lat
+seedy, seedx = ft.ui.picker.points(area, ft.vis.gca()).T
+seeds = [[x*111000, y*111000, 0, {'density':dens_larger}]
+            for x, y in zip(seedx, seedy)]
+ft.vis.show()
+
+ft.vis.figure()
+ft.vis.suptitle("Pick the seeds for the smaller portion")
+ft.vis.axis('scaled')
+ft.vis.contourf(lon, lat, data, (100, 100), 80, interp=True)
+ft.vis.colorbar()
+ft.vis.plot(outcropx, outcropy, '-k')
+seedy, seedx = ft.ui.picker.points(area, ft.vis.gca()).T
+seeds.extend([[x*111000, y*111000, 0, {'density':dens_smaller}]
+                for x, y in zip(seedx, seedy)])
+ft.vis.show()
+
+with open('seeds.json', 'w') as f:
+    json.dump(seeds, f)

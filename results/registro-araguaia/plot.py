5e7ac33a752a6628f184a4e588da4f65b5b59497
Leonardo Uieda <leouieda@gmail.com>
2012-12-10 17:08:23 -0200

    Added raw figures for the application results

--- results/registro-araguaia/plot.py
+++ results/registro-araguaia/plot.py
@@ -16,23 +16,23 @@ with open(os.path.join(path, 'seeds.pickle')) as f:
     seeds = pickle.load(f)
 sx, sy = np.transpose([s.center()[:2] for s in seeds])
 
-fmt = '.png'
+fmt = '.pdf'
 dpi = 200
 size = (5.6, 4)
 shape = (200, 200)
-
-ft.vis.figure(figsize=size)
-ft.vis.subplots_adjust(bottom=0.12)
-ft.vis.title("Bouguer anomaly")
-ft.vis.axis('scaled')
-levels = ft.vis.contourf(y, x, data, shape, 20, interp=True)
-cb = ft.vis.colorbar(shrink=0.86)
-cb.set_label('mGal')
-ft.vis.plot(y, x, 'xk')
-ft.vis.xlabel('East (km)')
-ft.vis.ylabel('North (km)')
-ft.vis.m2km()
-ft.vis.savefig('gz' + fmt, dpi=dpi)
+#
+#ft.vis.figure(figsize=size)
+#ft.vis.subplots_adjust(bottom=0.12)
+#ft.vis.title("Bouguer anomaly")
+#ft.vis.axis('scaled')
+#levels = ft.vis.contourf(y, x, data, shape, 20, interp=True)
+#cb = ft.vis.colorbar(shrink=0.86)
+#cb.set_label('mGal')
+#ft.vis.plot(y, x, 'xk')
+#ft.vis.xlabel('East (km)')
+#ft.vis.ylabel('North (km)')
+#ft.vis.m2km()
+#ft.vis.savefig('gz' + fmt, dpi=dpi)
 
 ft.vis.figure(figsize=size)
 ft.vis.subplots_adjust(bottom=0.12)
@@ -42,6 +42,7 @@ levels = ft.vis.contourf(y, x, data, shape, 20, interp=True)
 cb = ft.vis.colorbar(shrink=0.86)
 cb.set_label('mGal')
 ft.vis.plot(sy, sx, 'ok', markersize=8)
+ft.vis.plot(y, x, 'xk')
 ft.vis.xlabel('East (km)')
 ft.vis.ylabel('North (km)')
 ft.vis.m2km()
@@ -62,19 +63,43 @@ ft.vis.ylabel('North (km)')
 ft.vis.m2km()
 ft.vis.savefig('gz_fit' + fmt, dpi=dpi)
 
-ft.vis.show()
+#ft.vis.show()
 
 
 with open(os.path.join(path, 'result.pickle')) as f:
     mesh = pickle.load(f)
 bounds = mesh.bounds
 
-ft.vis.figure3d()
-ft.vis.prisms(seeds, 'density')
+def setview(scene):
+    scene.scene.camera.position = [4798.328726633199, -14151.146038974042, -257.13993504205513]
+    scene.scene.camera.focal_point = [74356.368529307525, 39415.820849083844, 17862.818922753184]
+    scene.scene.camera.view_angle = 30.0
+    scene.scene.camera.view_up = [0.16217628303946111, 0.12069320626390098, -0.97935284917196408]
+    scene.scene.camera.clipping_range = [23847.181091153507, 171909.1769857296]
+    scene.scene.camera.compute_view_plane_normal()
+    scene.scene.render()
+
+scene = ft.vis.figure3d(size=(1300, 800))
 ft.vis.prisms(ft.msh.ddd.vremove(0, 'density', mesh), 'density')
-#ft.vis.prisms(mesh, 'density')
-ft.vis.axes3d(ft.vis.outline3d(bounds), fmt='%.1f', nlabels=3,
-    ranges=[b*0.001 for b in bounds])
+ft.vis.outline3d(bounds)
 ft.vis.wall_bottom(bounds)
 ft.vis.wall_north(bounds)
+setview(scene)
+ft.vis.savefig3d('result.png')
+
+scene = ft.vis.figure3d(size=(1300, 800))
+ft.vis.prisms(seeds, 'density')
+ft.vis.outline3d(bounds)
+ft.vis.wall_bottom(bounds)
+ft.vis.wall_north(bounds)
+setview(scene)
+ft.vis.savefig3d('seeds.png')
+
+scene = ft.vis.figure3d(size=(1300, 800))
+ft.vis.prisms(seeds, 'density').actor.actor.visibility = 0
+ft.vis.axes3d(ft.vis.outline3d(bounds), fmt='%.1f', nlabels=3,
+    ranges=[b*0.001 for b in bounds])
+setview(scene)
+ft.vis.savefig3d('axes.pdf')
+
 ft.vis.show3d()
